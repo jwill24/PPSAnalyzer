d8bf8dcaa06f3b466d0e23799e1f2fae179742e7
Justin Williams <justinwilliams@ku.edu>
2020-06-17 13:19:31 -0500

    Generalized function for ratio plot of efficiencies or acceptance

--- plotResol.py
+++ plotResol.py
@@ -5,7 +5,7 @@ import math
 import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
-from ROOT import TCanvas, TPad, TFile, TPaveLabel, TPaveText, TAttText, TLine, TLegend, TBox, TColor, THStack, TGaxis, TH1F
+from ROOT import TCanvas, TPad, TFile, TPaveLabel, TPaveText, TAttText, TLine, TLegend, TBox, TColor, THStack, TGaxis, TH1F, TH1D
 from ROOT import gROOT, gStyle
 
 from common import Prettify
@@ -17,7 +17,9 @@ stations = [['0','45'], ['1','56'], ['3','45N'], ['23','45F'], ['103','56N'], ['
 
 #aqgcFile = TFile('outputHists/histOut_resolution_aqgc.root')
 #aqgcFile = TFile('histOut_signal_singleRP_2017postTS2.root')
-aqgcFile = TFile('output_hists_2017postTS2.root')
+
+f = TFile('outputHists/output_hists_2017postTS2.root')
+f_eff = TFile('outputHists/output_hists_2017postTS2_withWeights.root')
 
 def Canvas(name):
     c = TCanvas(name,'c',750,600)
@@ -48,6 +50,22 @@ def condLabel():
     label.SetTextFont( 42 )
     label.SetTextColor( 1 )
     return label
+
+def descriptionLabel():
+    label = TPaveText( 0.73, 0.65, 0.81, 0.8, 'NB NDC' )
+    label.SetFillStyle(0)
+    label.SetBorderSize(0)
+    label.SetLineWidth(0)
+    label.SetLineStyle(0)
+    label.AddText( "Elastic #gamma#gamma#rightarrow#gamma#gamma" )
+    label.AddText( "FPMC BSM pred." )
+    label.AddText( "#sigma_{bd} = 30 #murads" )
+    label.SetTextSize( 0.032 )
+    label.SetTextAlign(11)
+    label.SetTextFont( 42 )
+    label.SetTextColor( 1 )
+    return label
+
 
 def simLabel():
     label = TPaveText( 0.10, 0.9, 0.18, 0.92, 'NB NDC' )
@@ -118,43 +136,42 @@ def plotDiff(variable, symbol, h):
     sampleText.Draw(), cLabel.Draw(), pLabel.Draw(), statsLabel.Draw()
     c.SaveAs('plots/resolution/'+variable + '_difference.png')
 
-def plotEfficiency(var,method,numpro):
+#def plotEfficiency(var,method,numpro):
+def plotEA(h_selected,h_total,label_selected,label_total,fileOut):
     c = Canvas('c')
     pad1 = TPad('pad1', 'pad1', 0., 0.3, 1., 1.)
     pad1.SetBottomMargin(0.005)
     pad1.SetTicks(1,1)
+    pad1.SetGrid(1,1)
     pad1.Draw()
     c.cd()
-    pad2 = TPad('pad2', 'pad2', 0., 0.05, 1., 0.28)
+    pad2 = TPad('pad2', 'pad2', 0., 0.05, 1., 0.29)
     pad2.SetTopMargin(0.005)
     pad2.SetBottomMargin(0.3)
     pad2.SetTicks(1,1)
     pad2.Draw()
     pad1.cd()
-    #pad1.SetGrid(1,1)
-    h_total = aqgcFile.Get('h_'+var+'_sim')
     h_total.SetTitle('')
     h_total.GetYaxis().SetTitle('Events')
     h_total.SetLineColor(ROOT.kBlue)
-    h_total.GetYaxis().SetTitle('Events')
+    h_total.SetFillColorAlpha(ROOT.kBlue,0.2)
     h_total.Draw('HIST')
-    h_selected = aqgcFile.Get('h_'+var+'_sim_'+numpro+'pro_'+method)
     h_selected.SetTitle('')
     h_selected.SetLineColor(ROOT.kRed)
+    h_selected.SetFillColorAlpha(ROOT.kRed,0.2)
     h_selected.Draw('HIST same')
-    legend = TLegend(0.17, 0.57, 0.34, 0.77)
+    legend = TLegend(0.12, 0.61, 0.28, 0.8)
     legend.SetBorderSize(0)
     legend.SetFillColor(0)
     legend.SetFillStyle(0)
     legend.SetTextFont(42)
-    legend.SetTextSize(0.038)
-    if numpro == 'two': legend.AddEntry(h_selected,'Both protons reconstructed','l')
-    elif numpro == 'one': legend.AddEntry(h_selected,'Proton reconstructed ('+method+'RP)','l')
-    legend.AddEntry(h_total,'All events', 'l')
+    legend.SetTextSize(0.031)
+    legend.AddEntry(h_selected,label_selected, 'f')
+    legend.AddEntry(h_total,label_total, 'f')
     legend.Draw()
-    cLabel, pLabel = condLabel(), simLabel()
+    cLabel, pLabel, dLabel = condLabel(), simLabel(), descriptionLabel()
     cLabel.SetTextSize(0.049), pLabel.SetTextSize(0.049)
-    cLabel.Draw(), pLabel.Draw()
+    cLabel.Draw(), pLabel.Draw(), dLabel.Draw()
     pad2.cd()
     pad2.SetGrid(0,1)
     h_new = h_selected.Clone('h_new')
@@ -163,12 +180,13 @@ def plotEfficiency(var,method,numpro):
     h_new.SetMarkerStyle(20)
     h_new.SetMarkerSize(0.9)
     h_new.SetLineColor(ROOT.kBlack)
-    h_new.GetXaxis().SetTitle('Generated '+'#xi' if var=='xi' else 'missing mass')
+    h_new.GetXaxis().SetTitle('#xi' if 'xi' in fileOut else 'missing mass')
+    h_new.SetMinimum(-0.01), h_new.SetMaximum(1.05)
     h_new.Draw('p same')
     Prettify(h_new)
     #h_new.GetXaxis().SetTitle('Generated #xi')
     h_new.GetYaxis().SetTitle('Red/Blue')
-    c.SaveAs('test_efficiency_'+var+'_'+method+'.pdf')
+    c.SaveAs(fileOut)
 
 def plotProtonResolution(method,subdir):
     c = Canvas('c')
@@ -229,9 +247,38 @@ h_logxi_diff = aqgcFile.Get('plots/h_logxi_diff')
 plotDiff('logxi', 'log(1/#xi)', h_logxi_diff)
 '''
 
+h_xi_sim_total = f.Get('h_xi_sim')
+h_xi_sim_single = f.Get('h_xi_sim_twopro_single')
+h_xi_sim_multi = f.Get('h_xi_sim_twopro_multi')
+
+h_mass_sim_total = f.Get('h_mass_sim')
+h_mass_sim_single = f.Get('h_mass_sim_twopro_single')
+h_mass_sim_multi = f.Get('h_mass_sim_twopro_multi')
+
+h_xi_reco_total = TH1D('h_xi_reco_total', '', 100, 0.0, 0.3)
+h_xi_reco_eff = TH1D('h_xi_reco_eff', '', 100, 0.0, 0.3)
+h_xi_reco_total.Add( f.Get('multirp/0/h_xi_reco') )
+h_xi_reco_total.Add( f.Get('multirp/1/h_xi_reco') )
+h_xi_reco_eff.Add ( f_eff.Get('multirp/0/h_xi_reco') )
+h_xi_reco_eff.Add ( f_eff.Get('multirp/1/h_xi_reco') )
+
+h_mass_reco_total = f.Get('h_mass_reco')
+h_mass_reco_eff = f_eff.Get('h_mass_reco')
+
+#plotEA(h_xi_sim_single, h_xi_sim_total, 'Both singleRP reconstructed', 'All events', 'pds_xi_singleRP_acceptance.pdf')
+#plotEA(h_xi_sim_multi, h_xi_sim_total, 'Both multiRP reconstructed', 'All events', 'pds_xi_multiRP_acceptance.pdf')
+#plotEA(h_mass_sim_single, h_mass_sim_total, 'Both singleRP reconstructed', 'All events', 'pds_mass_singleRP_acceptance.pdf')
+#plotEA(h_mass_sim_multi, h_mass_sim_total, 'Both multiRP reconstructed', 'All events', 'pds_mass_multiRP_acceptance.pdf')
+
+plotEA(h_xi_reco_eff, h_xi_reco_total, 'multiRP with efficiencies', 'multiRP', 'pds_xi_efficiency.pdf')
+#plotEA(h_mass_reco_eff, h_mass_reco_total, 'multiRP with efficiencies', 'multiRP', 'pds_mass_efficiency.pdf')
+
+#plotEA(h_xi_reco_eff, h_xi_sim_total, 'multiRP reco with efficiencies', 'simulated', 'pds_xi_eXA.pdf')
+plotEA(h_mass_reco_eff, h_mass_sim_total, 'multiRP reco with efficiencies', 'simulated', 'pds_mass_eXA.pdf')
 
 
-plotEfficiency('mass','multi','two')
+# FIXME: outdated function
+#plotEfficiency('mass','multi','two')
 #plotEfficiency('mass','single','two')
 #plotEfficiency('xi','single','one')
 #plotEfficiency('xi','multi','one')
